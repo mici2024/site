af9b43b62e20ae149ca96ef45b917122b4239961
Acasandrei Mihai Cristian <mihaimtk96@gmail.com>
2024-09-12 23:05:30 +0300

    Added text highlighting in the index page

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
- const profiles = 
-[
+ const profiles = [
     {name: "Dino", image: "images/dino.jpg", link: "#Dino", alt: "Imagine cu Dino"},
     {name: "Lola", image: "images/lola.png", link: "#Lola", alt: "Imagine cu Lola"},
     {name: "Nick", image: "images/nick.jpg", link: "#Nick", alt: "Imagine cu Nick"},
@@ -16,24 +15,55 @@
 // Select the container where profiles will be inserted
 const profilesContainer = document.getElementById('profilesContainer');
 
+let html = '';
+
 // Generate HTML for each profile and append it to the container
-profiles.forEach( profile =>
-{
-        const profileItem = 
-        `
-            <a class="profileItem" href="${profile.link}">
+profiles.forEach(profile => {
+        html += `
+            <a class="profileItem internal-link" href="${profile.link}">
                 <div class="profileContent">
                     <img class="profilePicture" src="${profile.image}" alt="${profile.alt}">
                     <div class="name">${profile.name}</div>
                 </div>
             </a>
         `;
-
-    profilesContainer.innerHTML += profileItem;
 });
 
-const books = 
-[
+profilesContainer.innerHTML = html;
+
+function highlightElement() {
+    // removing the '#' from the anchor href
+    let targetId = location.hash.slice(1);
+
+    if (targetId) {
+        targetId += 'Text'; // for highlighting the text of the description
+
+        const targetElement = document.getElementById(targetId);
+        const highlightClassName = 'highlight';
+        const highlightDuration = 3000; // in milliseconds
+
+        if (targetElement) {
+            console.log(`Highlighting element with ID: ${targetId}`);
+            
+            targetElement.classList.add(highlightClassName);
+            setTimeout(() => targetElement.classList.remove(highlightClassName), highlightDuration);
+        }
+    } else {
+        console.log(`No element found with ID: ${targetId}`);
+    }
+}
+
+document
+    .querySelectorAll('a.internal-link[href^="#"]')
+    .forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            location.hash = this.getAttribute('href');
+            highlightElement();
+        });
+    });
+
+const books = [
     {image: "images/inteligenta-emotionala.jpeg", link: "https://cartigratis.com/products/3851-inteligenta-emotionala", alt: "Inteligenta emotionala"},
     {image: "images/gandeste-si-devii-bogat.jpg", link: "https://cartigratis.com/products/3584-gandeste-si-devii-bogat", alt: "Gandeste si devii bogat"},
     {image: "images/cele-cinci-limbaje-de-iubire.jpg", link: "https://cartigratis.com/products/3975-cele-cinci-limbaje-de-iubire-ale-copiilor", alt: "Cele cinci limbaje de iubire ale copiilor"},
@@ -45,15 +75,12 @@ const books =
 
 const booksContainer = document.getElementById('bookContainer');
 
-books.forEach(book =>
-    {
-        const bookItem = 
-        `
-            <a href="${book.link}" target="_blank">
-                <img class="fixed-size-image image-frame" src="${book.image}" alt="${book.alt}"/>
-            </a>
-        `;
+books.forEach(book => {
+    const bookItem = `
+        <a href="${book.link}" target="_blank">
+            <img class="fixed-size-image image-frame" src="${book.image}" alt="${book.alt}"/>
+        </a>
+    `;
 
-        booksContainer.insertAdjacentHTML('beforeend', bookItem);
-    }
-)
+    booksContainer.insertAdjacentHTML('beforeend', bookItem);
+});
