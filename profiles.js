709a9b1079d46aee8f3bdb002dd21601c7f3b895
Acasandrei Mihai Cristian <mihaimtk96@gmail.com>
2024-09-13 13:47:06 +0300

    Organized JS files

--- profiles.js
+++ profiles.js
@@ -12,13 +12,13 @@
     {name: "Maghi", image: "images/maghi.jpg", link: "#Maghi", alt: "Imagine cu Maghi"}
 ];
 
-// Select the container where profiles will be inserted
-const profilesContainer = document.getElementById('profilesContainer');
+function renderProfiles(containterId, profiles) {
+    // Select the container where profiles will be inserted
+    const profilesContainer = document.getElementById(containterId);
+    let html = '';
 
-let html = '';
-
-// Generate HTML for each profile and append it to the container
-profiles.forEach(profile => {
+    // Generate HTML for each profile and append it to the container
+    profiles.forEach(profile => {
         html += `
             <a class="profileItem internal-link" href="${profile.link}">
                 <div class="profileContent">
@@ -27,16 +27,19 @@ profiles.forEach(profile => {
                 </div>
             </a>
         `;
-});
+    });
 
-profilesContainer.innerHTML = html;
+    profilesContainer.innerHTML = html;
+    addProfileEventListeners();
+}
 
 function highlightElement() {
-    // removing the '#' from the anchor href
+     // removing the '#' from the anchor href
     let targetId = location.hash.slice(1);
 
     if (targetId) {
-        targetId += 'Text'; // for highlighting the text of the description
+         // highlighting the text of the description
+        targetId += 'Text';
 
         const targetElement = document.getElementById(targetId);
         const highlightClassName = 'highlight';
@@ -53,34 +56,16 @@ function highlightElement() {
     }
 }
 
-document
+function addProfileEventListeners() {
+    document
     .querySelectorAll('a.internal-link[href^="#"]')
     .forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
+        anchor.addEventListener('click', e => {
             e.preventDefault();
-            location.hash = this.getAttribute('href');
+            location.hash = anchor.getAttribute('href');
             highlightElement();
         });
     });
+}
 
-const books = [
-    {image: "images/inteligenta-emotionala.jpeg", link: "https://cartigratis.com/products/3851-inteligenta-emotionala", alt: "Inteligenta emotionala"},
-    {image: "images/gandeste-si-devii-bogat.jpg", link: "https://cartigratis.com/products/3584-gandeste-si-devii-bogat", alt: "Gandeste si devii bogat"},
-    {image: "images/cele-cinci-limbaje-de-iubire.jpg", link: "https://cartigratis.com/products/3975-cele-cinci-limbaje-de-iubire-ale-copiilor", alt: "Cele cinci limbaje de iubire ale copiilor"},
-    {image: "images/ce-sa-le-spunem-copiilor.jpg", link: "https://cartigratis.com/products/3955-ce-sa-le-spunem-copiilor", alt: "Ce sa le spunem copiilor"},
-    {image: "images/drumul-catre-tine-insuti.jpg", link: "https://cartigratis.com/products/3582-drumul-catre-tine-insuti", alt: "Drumul catre tine insuti"},
-    {image: "images/secretele-comunicarii.jpg", link: "https://cartigratis.com/products/3583-secretele-comunicarii", alt: "Secretele comunicarii"},
-    {image: "images/limbajul-trupului.jpeg", link: "https://cartigratis.com/products/3602-limbajul-vorbirii", alt: "Limbajul trupului"},
-];
-
-const booksContainer = document.getElementById('bookContainer');
-
-books.forEach(book => {
-    const bookItem = `
-        <a href="${book.link}" target="_blank">
-            <img class="fixed-size-image image-frame" src="${book.image}" alt="${book.alt}"/>
-        </a>
-    `;
-
-    booksContainer.insertAdjacentHTML('beforeend', bookItem);
-});
+renderProfiles('profilesContainer', profiles);
